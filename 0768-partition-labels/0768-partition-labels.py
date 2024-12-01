b61661c0b789123b215aa292af05185fde11ece5
Amanuel <113626608+Zeckaris@users.noreply.github.com>
2024-12-01 22:35:33 +0300

    Time: 4 ms (67.66%), Space: 17.2 MB (5.38%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -2,25 +2,21 @@ class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         letterDict= dict()
         for i in range(len(s)):
-            if s[i] not in letterDict:
-                letterDict[s[i]] = [i,-1]
-            else:
-                letterDict[s[i]][1]= i
+            letterDict[s[i]] = i
+
         ans=[]
         begin=0
-        end=-1
+        end=0
         start=0
         while begin < len(s):
-            end= max(letterDict[s[begin]][1], end)
-            if end == -1:
-                ans.append(1)
-                start= begin + 1
-            else:
-                if end ==  begin:
-                    ans.append(end - start + 1)
-                    start= end + 1
-                    end= -1
+            end= max(letterDict[s[begin]], end)
+            if end ==  begin:
+                ans.append(end - start + 1)
+                start= end + 1
+                end= 0
+
             begin += 1
+
         return ans
 
 
