fbce460f4dfaf7346c1ce63cc5449dbe482afc4d
Amanuel <113626608+Zeckaris@users.noreply.github.com>
2025-03-28 15:23:48 +0300

    Time: 4 ms (12.97%), Space: 18.2 MB (19.37%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.py
+++ 0682-baseball-game/0682-baseball-game.py
@@ -5,17 +5,15 @@ class Solution:
             try:
                 num= int(i)
                 stack.append(num)
-            except:
-                if stack:
-                    if i == "C":
-                        stack.pop()
-                    elif i == "D":
-                        temp= 2 * stack[-1]
-                        stack.append(temp)
-                    else:
-                        temp= stack[-2] + stack[-1]
-                        stack.append(temp)
-            print(stack)    
+            except ValueError:
+                if i == "C":
+                    stack.pop()
+                elif i == "D":
+                    temp= 2 * stack[-1]
+                    stack.append(temp)
+                else:
+                    temp= stack[-2] + stack[-1]
+                    stack.append(temp)   
         return sum(stack)
 
 
