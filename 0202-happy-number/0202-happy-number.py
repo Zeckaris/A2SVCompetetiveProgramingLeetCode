aba6b39a300263d6a64fd08d758352fd5d6a021d
Amanuel <113626608+Zeckaris@users.noreply.github.com>
2025-03-30 11:39:17 +0300

    Time: 4 ms (16.47%), Space: 18.1 MB (17.48%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -2,27 +2,18 @@ from typing import List
 class Solution:
     def isHappy(self, n: int) -> bool:
         def partition(n:int)->List[int]:
-            lst=[]
+            summation=0
             while n >= 1:
                 modulo= n % 10
+                summation= summation + pow(modulo, 2)
                 n= n // 10
-                lst.append(modulo)
-            summation= 0
-            for i in lst:
-                summation += pow(i,2)
             return summation
-        cycleDetect= set()
-        check= n
-        while True:
-            check= partition(check)
-            if check == 1:
-                return True
-            elif check in cycleDetect:
-                return False
-            cycleDetect.add(check)
-        return False
 
+        slow, fast= n, partition(n)
 
-        
+        while slow != fast and fast != 1:
+            slow= partition(slow)
+            fast = partition(partition(fast))
 
-        
+            
+        return fast==1
